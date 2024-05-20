c29a8edf9e599f047b8bba2bc336aaa2fc478eac
Bangwahyu14 <whagung553@gmail.com>
2024-05-20 18:28:26 +0700

    menambahkan pasien string pnama

--- memberlist/Asosiasi.cpp
+++ memberlist/Asosiasi.cpp
@@ -6,8 +6,10 @@ class dokter;
 class pasien {//asosiasi
 public:
 	string nama;
-	vector<dokter*> daftar_dokter
-
+	vector<dokter*> daftar_dokter;
+	pasien(string pNama) :nama(pNama) {
+		cout << "pasien \"" << nama << "\" tidak ada\n";
+	}
 
 
 };
