8c84cc42422313a493d213073441d310c1bd7a1e
Bangwahyu14 <whagung553@gmail.com>
2024-05-20 19:09:12 +0700

    menambahkan void pasien

--- memberlist/Asosiasi.cpp
+++ memberlist/Asosiasi.cpp
@@ -33,3 +33,10 @@ public:
 void pasien::tambahDokter(dokter* pDokter) {
 	daftar_dokter.push_back(pDokter);
 }
+void pasien::cetakDokter() {
+	cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+	for (auto& a : daftar_dokter) {  // auto digunakan dalam perulangan for untuk secara otomatis menentukan tipe data daru elemen yang diiterasi
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
